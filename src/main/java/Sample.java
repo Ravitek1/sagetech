460f549c58113116306bc1a303f74e58169eff8d
ravi <ravitek1@umbc.edu>
2024-01-16 19:31:24 -0600

    version 3

--- src/main/java/Sample.java
+++ src/main/java/Sample.java
@@ -2,7 +2,7 @@ public class Sample {
     public static void main(String[] args) {
         Example exp1 = new Example();
         Example exp2 = new Example();
-        System.out.println("git hellllo" + exp1.getExamplename() +" exap2 number"+exp1.getExampleNo());
+        System.out.println("dell hellllo" + exp1.getExamplename() +" exap2 number"+exp1.getExampleNo());
         System.out.println("git hellllo" + exp2.getExamplename() +" exap2 number"+exp2.getExampleNo()  );
     }
 }
