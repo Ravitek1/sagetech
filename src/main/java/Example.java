20ad8a3cb1f035ebe9f4b29d512d65b1d0a8491b
ravi <ravitek1@umbc.edu>
2024-01-16 18:04:02 -0600

    intial commit version 1

---
+++ src/main/java/Example.java
@@ -0,0 +1,18 @@
+public class Example {
+    public String getExamplename() {
+        return examplename;
+    }
+
+
+
+    public int getExampleNo() {
+        return exampleNo;
+    }
+
+
+
+    private String examplename = " sample example";
+    private int exampleNo = 1;
+    public static int sample =25;
+
+}
--- src/main/java/Sample.java
+++ src/main/java/Sample.java
@@ -1,5 +1,8 @@
 public class Sample {
     public static void main(String[] args) {
-        System.out.println("git hellllo");
+        Example exp1 = new Example();
+        Example exp2 = new Example();
+        System.out.println("git hellllo" + exp1.getExamplename() +" exap2 number"+exp1.getExampleNo());
+        System.out.println("git hellllo" + exp2.getExamplename() +" exap2 number"+exp2.getExampleNo() +""+Example.sample );
     }
 }
