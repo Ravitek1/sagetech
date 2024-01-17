9ccdd6f4c01df426d9cd57abf693e6588117d92f
ravi <ravitek1@umbc.edu>
2024-01-16 18:23:26 -0600

    modifcation to version 1

--- src/main/java/Sample.java
+++ src/main/java/Sample.java
@@ -3,6 +3,6 @@ public class Sample {
         Example exp1 = new Example();
         Example exp2 = new Example();
         System.out.println("git hellllo" + exp1.getExamplename() +" exap2 number"+exp1.getExampleNo());
-        System.out.println("git hellllo" + exp2.getExamplename() +" exap2 number"+exp2.getExampleNo() +""+Example.sample );
+        System.out.println("git hellllo" + exp2.getExamplename() +" exap2 number"+exp2.getExampleNo()  );
     }
 }
